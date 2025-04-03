97d58a2f209e64b69449a804b9a19aeb11f15bd0
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2025-04-02 21:04:55 -0700

    create red cube with Orbit Controls

--- src/script.js
+++ src/script.js
@@ -1,30 +1,45 @@
-import * as THREE from 'three';
-
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+console.log(OrbitControls);
 // initialize the scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 
-const cubeMesh = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
-scene.add(cubeMesh)
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
-  75, 
+  35,
   window.innerWidth / window.innerHeight,
   0.1,
-  30)
-camera.position.z = 5
+  200
+);
+camera.position.z = 5;
 
 // initialize the renderer
-const canvas = document.querySelector('canvas.threejs')
+const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+//initialize the orbit controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+controls.autoRotate = true;
+//instantiate the renderer
+const renderloop = () => {
+  // update the controls
+  controls.update();
+  // render the scene
+  renderer.render(scene, camera);
+
+  // call the next frame
+  window.requestAnimationFrame(renderloop);
+};
+// start the render loop
+renderloop();
